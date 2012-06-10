89e3965896daa56e38cfe42883fddb36ccf53199
Nick Chadwick <chadnickbok@gmail.com>
2012-06-09 18:27:23 -0700

    Made nose and mouth detection more accurate.

--- fancify.py
+++ fancify.py
@@ -14,14 +14,15 @@ from flask import request
 import SimpleCV
 from SimpleCV import Color, ColorCurve, Camera, Image, pg, np, HaarCascade
 from SimpleCV.Display import Display
+from SimpleCV import DrawingLayer
 
 app = Flask(__name__)
 app.debug = True
 
 face_cascade = HaarCascade("/var/lib/SimpleCV/Features/HaarCascades/face.xml")
 nose_cascade = HaarCascade("/var/lib/SimpleCV/Features/HaarCascades/nose.xml")
+mouth_cascade = HaarCascade("/var/lib/SimpleCV/Features/HaarCascades/mouth.xml")
 stache = Image("./stache.png")
-mask = stache.createAlphaMask(hue_lb=60, hue_ub=130).invert()
 
 @app.route('/')
 def hello_world():
@@ -41,23 +42,77 @@ def fancify():
     faces = img.findHaarFeatures(face_cascade)
     if faces is not None:
         for face in faces:
+            nose = None
+            mouth = None
             cur_face = face.crop()
             noses = cur_face.findHaarFeatures(nose_cascade)
+            mouths = cur_face.findHaarFeatures(mouth_cascade)
+            print "X:", face.x
+            print "Y:", face.y
+            face_left_edge = face.x - (face.width() / 2)
+            face_top_edge = face.y - (face.height() / 2)
 
             if noses is not None:
-                noses = noses.sortArea()
                 nose = noses[0]
+                nose_dist = abs(face_left_edge + nose.x - face.x) + abs(face_top_edge + nose.y - face.y)
+                for cur_nose in noses:
+                    cur_dist = abs(face_left_edge + cur_nose.x - face.x) + abs(face_top_edge + cur_nose.y - face.y)
+                    if cur_dist < nose_dist:
+                        nose = cur_nose
+                        nost_dist = cur_dist
 
-                xf = face.x -(face.width()/2)
-                yf = face.y -(face.height()/2)
-                xm = nose.x -(nose.width()/2)
-                ym = nose.y -(nose.height()/2)
-                #calculate the mustache position
-                xmust = xf+xm-(stache.width/2)+(nose.width()/2)
-                ymust = yf+ym+(2*nose.height()/3)
+            if mouths is not None:
+                mouth = mouths[0]
+                mouth_dist = abs(face_left_edge + mouth.x - face.x) + abs(face_top_edge + mouth.y - (face.y + (face.height() / 4)))
+                for cur_mouth in mouths:
+                    cur_dist = abs(cur_mouth.x - face.x) + abs(cur_mouth.y - (face.y + (face.height() / 4)))
+                    if cur_dist < mouth_dist:
+                        mouth = cur_mouth
+                        mouth_dist = cur_dist
+
+            if nose and mouth:
+
+                x_face = face.x - (face.width() / 2)
+                y_face = face.y - (face.height() / 2)
+
+                x_nose = nose.x - (nose.width() / 2)
+                y_nose = nose.y - (nose.height() / 2)
+
+                x_mouth = mouth.x - (mouth.width() / 2)
+                y_mouth = mouth.y - (mouth.height() / 2)
 
                 #blit the stache/mask onto the image
-                img = img.blit(stache, pos=(xmust,ymust), alphaMask=mask)
+                cur_stache = stache.copy()
+                scale_factor = nose.width() / 35.0
+                cur_stache = cur_stache.scale(scale_factor)
+                mask = cur_stache.createAlphaMask(hue_lb=60, hue_ub=130).invert()
+
+                #calculate the mustache position
+                x_must = x_face + x_mouth + (mouth.width() / 2) - (cur_stache.width / 2)
+                y_must = y_face + y_mouth - (cur_stache.height / 2)
+
+                facelayer = DrawingLayer((img.width, img.height))
+                facebox_dimensions = (face.width(), face.height())
+                center_point = (face.x, face.y)
+                facebox = facelayer.centeredRectangle(center_point, facebox_dimensions)
+                #img.addDrawingLayer(facelayer)
+
+                noselayer = DrawingLayer((img.width, img.height))
+                nosebox_dimensions = (nose.width(), nose.height())
+                center_point = (face.x - (face.width() / 2) + nose.x,
+                                    face.y - (face.height() / 2) + nose.y)
+                nosebox = noselayer.centeredRectangle(center_point, nosebox_dimensions)
+                #img.addDrawingLayer(noselayer)
+
+                mouthlayer = DrawingLayer((img.width, img.height))
+                mouthbox_dimensions = (mouth.width(), mouth.height())
+                center_point = (face.x - (face.width() / 2) + mouth.x,
+                                face.y - (face.height() / 2) + mouth.y)
+                mouthbox = mouthlayer.centeredRectangle(center_point, mouthbox_dimensions)
+                #img.addDrawingLayer(mouthlayer)
+
+                #img = img.applyLayers()
+                img = img.blit(cur_stache, pos=(x_must, y_must), alphaMask=mask)
 
     output = StringIO.StringIO()
     img.getPIL().save(output, format="JPEG")
